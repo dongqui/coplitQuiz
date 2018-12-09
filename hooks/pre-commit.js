a27cc967ef1e92e1ab0a51abae9ed680bd82a5a6
dongqui <kimwinner90@gmail.com>
2018-12-09 18:04:17 +0900

    asd

--- hooks/pre-commit.js
+++ hooks/pre-commit.js
@@ -2,7 +2,6 @@
   const {classroomList, allQuizList, dockerimageList} = require('../FolderFairy/util/listFactory');
   let commitAddedData = process.argv.slice(2);
   let dataWithFlag = commitAddedData.filter(commit => commit.includes('flag'));
-  console.log(dataWithFlag);
   for (let i = 0; i < dataWithFlag.length; i++) {
     let data = dataWithFlag[i].split('/');
     if (data.length === 5) {
@@ -13,7 +12,6 @@
 
   async function checkIfQuizNameExist(data) {
     let [addedDockerImage, addedClassRoom, addedQuiz] = data.slice(1, 4);
-    console.log(data);
     let dockerImageList = await dockerimageList();
     let dockerImageListWithOutAddedOne = dockerImageList.filter(dockerimage => dockerimage !== addedDockerImage);
     for (let i = 0; i < dockerImageListWithOutAddedOne.length; i++) {
@@ -21,10 +19,11 @@
       let classRoomList = await classroomList(dockerImage);
       if (classRoomList.includes(addedClassRoom)) {
         let quizList = await allQuizList(dockerImage, addedClassRoom);
-        console.log(dockerImage, addedClassRoom, addedQuiz);
         if (quizList.includes(addedQuiz)) {
-          console.log('커밋 실패!')
-          console.log(`${addedClassRoom}에 중복되는 문제가 존재합니다. 다시 확인해주세요`);
+          console.log(`
+          커밋 실패!
+          ${addedClassRoom}에 중복되는 문제가 존재합니다. 다시 확인해주세요
+          `);
           process.exit(1);
         }
       }
