d56f75c83f4392d5ea0f1f94cf855dd9090707af
dongqui <kimwinner90@gmail.com>
2018-12-10 00:50:26 +0900

    test

--- hooks/pre-commit.js
+++ hooks/pre-commit.js
@@ -11,7 +11,7 @@
   }
 
   async function checkIfQuizNameExist(data, allQuizObj) {
-
+    console.log(`${data} 중복 검사...`);
     let [addedDockerImage, addedClassRoom, addedQuiz] = data.slice(1, 4);
     let dockerImageListWithOutAddedOne = Object.keys(allQuizObj).filter(dockerimage => dockerimage !== addedDockerImage);
     let isDuplicated = dockerImageListWithOutAddedOne.some(dockerImage =>
@@ -21,8 +21,9 @@
           커밋 실패!
           ${addedClassRoom}에 중복되는 문제가 존재합니다. 다시 확인해주세요
           `);
+      process.exit(1);
     }
-    process.exit(1);
+    console.log('ok');
   }
 
 })();
